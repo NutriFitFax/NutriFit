f1be7cd49e2e8ded3ac62343a00ed2dcdecbd6d8
ahmedm1921 <230302265@student.ius.edu.ba>
2026-05-31 00:51:56 +0200

    Create auth_gate.dart

---
+++ mobile/lib/features/auth/auth_gate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../features/auth/login_screen.dart';
+import '../app/app_shell.dart';
+import '../app/settings_prefs.dart';
+import '../api/api_client.dart';
+import '../features/history/viewed_food_history_store.dart';
+
+class AuthGate extends StatelessWidget {
+  final NutriFitApi api;
+  final ViewedFoodHistoryStore history;
+
+  const AuthGate({
+    super.key,
+    required this.api,
+    required this.history,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isLoggedIn = SettingsPrefs.instance.getUserEmail() != null;
+
+    if (!isLoggedIn) {
+      return LoginScreen(
+        onLogin: (email) {
+          SettingsPrefs.instance.setUserEmail(email);
+        },
+        onRegister: () {},
+      );
+    }
+
+    return AppShell(api: api, history: history);
+  }
+}
